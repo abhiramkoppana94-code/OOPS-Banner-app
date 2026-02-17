66ee12bc7b30589681e200697ae978b6d5d7d4ed
Abhiram <abhiramkoppana94@gmail.com>
2026-02-17 15:42:53 +0530

    .

--- OOPS-Banner-app/Oops.java
+++ OOPS-Banner-app/Oops.java
@@ -1,11 +1,14 @@
 public class Oops {
-    public static void main(String[] args) {
-     System.out.println("   ***   " + "  " + "   *** ");
-	 System.out.println(" **   ** " + "  " + " **      ");
-	 System.out.println("**     **" + "  " + "**       ");
-	 System.out.println("**     **" + "  " + "  *****  ");
-     System.out.println("**     **" + "  " + "**     **");
-     System.out.println("**    ** " + "  " + "**    ** ");
-     System.out.println("   ***   " + "  " + "  *****  ");
-	 }
-	 }
+     public static void main(String[] args) {
+
+        System.out.println("  *****    *****    ******    ****** ");
+        System.out.println(" *     *  *     *   *     *  *       ");
+        System.out.println(" *     *  *     *   *     *  *       ");
+        System.out.println(" *     *  *     *   ******    *****  ");
+        System.out.println(" *     *  *     *   *              * ");
+        System.out.println(" *     *  *     *   *              * ");
+        System.out.println("  *****    *****    *         ****** ");
+
+    }
+    
+}
